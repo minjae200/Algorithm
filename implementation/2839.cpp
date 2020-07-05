113de0b6e0dd5352c5b605d6a1aff046cf1ed4c4
minjae.choi <chlalswo200@naver.com>
2020-07-05 20:52:05 +0900

    설탕 배달

---
+++ implementation/2839.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void){
+    int s, B = 0;
+    scanf("%d", &s);
+    while (1) {
+        if (s % 5 == 0) {
+            printf("%d", s/5 + B);
+            break;
+        }
+        else if (s <= 0) {
+            printf("-1");
+            break;
+        }
+        s -= 3;
+        B++;
+    }
+    return 0;
+}
