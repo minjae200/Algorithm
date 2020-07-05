4f52cc5008d8307366eed955c0662326135b13cd
minjae.choi <chlalswo200@naver.com>
2020-07-05 20:56:53 +0900

    베르트랑 공준

---
+++ implementation/4948.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int a[246913], n;
+int main() {
+	a[1] = 1;
+	for (int i = 2; i <= 246912; i++) {
+		if (a[i] == 1)
+			continue;
+		for (int j = i + i; j <= 246912; j += i)
+			a[j] = 1;
+	}
+
+	while (1) {
+		cin >> n;
+		if (!n)
+			break;
+		int c = 0;
+		for (int i = n + 1; i <= 2 * n; i++)
+			if (!a[i])
+				c++;
+		cout << c << '\n';
+	}
+
+	return 0;
+}
